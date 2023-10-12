d4a01b1410900346d7f353f494ac04b7f22ef127
Chiennnps27765 <chiennguyen2841998@gmail.com>
2023-10-12 15:32:45 +0700

    commit lần 3

--- Template_QLBanHang_chiennnps27765_Fall23/1_DAL_QLBanHang/DatabaseConnect.cs
+++ Template_QLBanHang_chiennnps27765_Fall23/1_DAL_QLBanHang/DatabaseConnect.cs
@@ -6,9 +6,14 @@ namespace _1_DAL_QLBanHang
 {
     public class DatabaseConnect
     {
-        private static string directoryPath = AppDomain.CurrentDomain.BaseDirectory;
+        // Kết nối dành cho localDb
+        /*private static string directoryPath = AppDomain.CurrentDomain.BaseDirectory;
         private static string mdfFilePath = Path.Combine(directoryPath, "QLBanHang.mdf");
         private static string connection = $@"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename = {mdfFilePath} ;Initial Catalog=QLBanHang;Integrated Security=True";
-        protected SqlConnection _Conn = new SqlConnection(connection);
+        protected SqlConnection _Conn = new SqlConnection(connection);*/
+
+        // Kết nối đến Azure Sql
+        private const string azureConnection = @"Server=tcp:chienprivate.database.windows.net,1433;Initial Catalog=QuanLyBanHang;Persist Security Info=False;User ID=chienprivate@gmail.com@chienprivate;Password=Chienprivate@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        protected SqlConnection _Conn = new SqlConnection(azureConnection);
     }
 }
